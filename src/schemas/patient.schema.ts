0eb6ed895adec87cd4cdb3afa252daf2ddaf16e9
SusimaraRoberti <susimararoberti@outlook.com>
2022-09-06 19:42:02 -0300

    fix: alterado idioma do schema patients

--- src/schemas/patient.schema.ts
+++ src/schemas/patient.schema.ts
@@ -3,30 +3,30 @@ import * as yup from "yup";
 const patientSchema = yup.object().shape({
   name: yup
     .string()
-    .required("Nome e Sobrenome obrigatórios")
+    .required("Mandatory first and last name")
     .matches(
       /^[A-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-ZÀ-Ÿ][A-zÀ-ÿ']+$/,
-      "Primeira letra de cada nome maiúscula, " +
-        "sem espaços duplos " +
-        "e sem números"
+      "First letter of each name capitalized, " +
+        "no double spaces, " +
+        "and without numbers."
     ),
-  img: yup.string().required("Insira a url da sua foto de perfil"),
+  img: yup.string().required("Enter your profile picture url."),
   age: yup
     .number()
-    .typeError("Apenas números")
+    .typeError("Only numbers")
     .required()
-    .positive("Apenas números positivos")
-    .integer("Apenas números inteiros"),
+    .positive("Only positive numbers")
+    .integer("Only whole numbers"),
   status: yup.string().required(),
   schooling: yup.string().required(),
   profession: yup
     .string()
-    .min(5, "Mínimo 5 caracteres")
-    .required("Campo obrigatório!"),
+    .min(5, "Minimum 5 characters")
+    .required("Required field!"),
   complaint: yup
     .string()
-    .required("Campo obrigatório!")
-    .min(10, "Mínimo 10 caracteres"),
+    .required("Required field!")
+    .min(10, "Minimum 10 characters"),
   medication: yup.string(),
   disease: yup.string(),
 });
