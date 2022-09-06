a7bbbc5904963ebaa9d7afd0c20e29d4a798c9a1
AlexFlavio <alexflavio.dev@gmail.com>
2022-09-06 17:51:06 -0300

    fix_listPsychologist: adicionados cotrollers em suas respectivas rotas.

--- src/routes/psychologist.routes.ts
+++ src/routes/psychologist.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import createPsychologistPerfilController from "../controllers/psychologist/createPsychologistPerfil.controller";
+import listPatientsController from "../controllers/psychologists/listPatients.controller";
 import { validateSchemaMiddleware } from "../middlewares/validateSchema.middleware";
 import { ensureAuth } from "../middlewares/validateToken.middleware";
 import { validateUserType } from "../middlewares/validateUserType.middleware";
 import psychologistSchema from "../schemas/psychologist.schema";
+import { psychologist } from "../utils/utils";
 
 const psyRouter = Router();
 
@@ -13,6 +15,12 @@ psyRouter.post(
   ensureAuth,
   validateUserType,
   createPsychologistPerfilController
+);
+psyRouter.get(
+  "",
+  ensureAuth,
+  validateUserType(psychologist),
+  listPatientsController
 );
 
 export default psyRouter;
