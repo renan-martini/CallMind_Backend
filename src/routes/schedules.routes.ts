0c5f438db4b7b7c1e7de9d20ba6962584b5af389
AlexFlavio <alexflavio.dev@gmail.com>
2022-09-07 11:39:13 -0300

    feature: editSchedules , foi criado service e controller para edição de scheduldules(agendamento) e adiconado o controller as rotas de schedules, ainda tem de ser testado!

--- src/routes/schedules.routes.ts
+++ src/routes/schedules.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { validate } from "uuid";
+import editScheduleController from "../controllers/schedules/editSchedules.controller";
 import listSchedulesController from "../controllers/schedules/listSchedules.controller";
 import { validateUserFirstLogin } from "../middlewares/validateFirstLogin.middleware";
 import { ensureAuth } from "../middlewares/validateToken.middleware";
+import { validateUserType } from "../middlewares/validateUserType.middleware";
+import { patient } from "../utils/utils";
 
 const schedulesRouter = Router();
 
@@ -11,6 +14,13 @@ schedulesRouter.get(
   ensureAuth,
   validateUserFirstLogin,
   listSchedulesController
+);
+schedulesRouter.post(
+  "/:id",
+  ensureAuth,
+  validateUserFirstLogin,
+  validateUserType(patient),
+  editScheduleController
 );
 
 export default schedulesRouter;
