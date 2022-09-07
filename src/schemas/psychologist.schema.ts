7b500ee6e899cb1225b0bd92db835ec2959e393f
SusimaraRoberti <susimararoberti@outlook.com>
2022-09-07 16:08:10 -0300

    fix: realizado melhoria do schema de criação de psychologist

--- src/schemas/psychologist.schema.ts
+++ src/schemas/psychologist.schema.ts
@@ -1,13 +1,30 @@
 import * as yup from "yup";
 
 const psychologistSchema = yup.object().shape({
-  name: yup.string().required(),
-  img: yup.string().required(),
-  emphasis: yup.string().required(),
-  experience: yup.string().required(),
+  name: yup
+    .string()
+    .required("Mandatory first and last name")
+    .matches(
+      /^[A-ZÀ-Ÿ][A-zÀ-ÿ']+\s([A-zÀ-ÿ']\s?)*[A-ZÀ-Ÿ][A-zÀ-ÿ']+$/,
+      "First letter of each name capitalized, " +
+        "no double spaces, " +
+        "and without numbers."
+    ),
+  img: yup.string().required("Enter your profile picture url."),
+  emphasis: yup
+    .string()
+    .min(10, "Minimum 10 characters")
+    .required("Required field!"),
+  experience: yup
+    .string()
+    .min(5, "Minimum 5 characters")
+    .required("Required field!"),
   available_times: yup.string().required(),
   working_days: yup.array().required(),
-  registration: yup.string().required(),
+  registration: yup
+    .string()
+    .min(6, "Minimum 6 characters")
+    .required("Required field!"),
 });
 
 export default psychologistSchema;
