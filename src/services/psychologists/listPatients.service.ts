9ab9259ed361eb26df3d84b186cdd3fef4d5f161
AlexFlavio <alexflavio.dev@gmail.com>
2022-09-07 21:09:27 -0300

    fix: listPatients, estava buscando utilizando userId ao invés do psychologist Id, para resolver fiz um findOne do psicologo para estar passando o id correto dentro do query builder para compararação, tambem foi feita uma alteração para só fazer a busca pelos que já foram prenchidos, ainda tem de ser testado!

--- src/services/psychologists/listPatients.service.ts
+++ src/services/psychologists/listPatients.service.ts
@@ -1,14 +1,22 @@
 import AppDataSource from "../../data-source";
+import { Psychologist } from "../../entities/psychologist.entity";
 
 import { Schedule } from "../../entities/schedule.entity";
 
 const listPatientsService = async (id: string) => {
+  const psychologistRepository = AppDataSource.getRepository(Psychologist);
+  const psychologist = await psychologistRepository.findOne({
+    relations: { user: true },
+    where: { user: { id: id } },
+  });
   const patients = await AppDataSource.getRepository(Schedule)
     .createQueryBuilder("schedules")
     .innerJoinAndSelect("schedules.patient", "patient")
     .innerJoinAndSelect("schedules.psychologist", "psychologist")
     .select(["patient.*"])
-    .where("psychologist.id = :id", { id })
+    .where("psychologist.id = :id, schedules.available = false", {
+      id: psychologist!.id,
+    })
     .getMany();
   return patients;
 };
