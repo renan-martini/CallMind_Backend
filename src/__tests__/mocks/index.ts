58a064868c2c0802b5c329262633d8139b9e75c5
SusimaraRoberti <susimararoberti@outlook.com>
2022-09-08 16:32:26 -0300

    feat: inseridas as configurações e pastas para montagens dos testes

---
+++ src/__tests__/mocks/index.ts
@@ -0,0 +1,15 @@
+import { IUserRequest, IUserLogin } from "../../interfaces/users";
+import { IPatientRequest } from "../../interfaces/patients";
+import { IUserPsychologistRequest } from "../../interfaces/psychologists";
+import { IScheduleCreateRequest } from "../../interfaces/schedules";
+import { IChart } from "../../interfaces/chart";
+
+//export const mockedUser : IUserRequest = {}
+
+//export const mockedPatient : IPatientRequest = {}
+
+//export const mockedPsuchologist : IUserPsychologistRequest = {}
+
+//export const mockedSchedule : IScheduleCreateRequest = {}
+
+//export const mockedChart : IChart = {}
--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ const AppDataSource = new DataSource(
         type: "sqlite",
         database: ":memory:",
         synchronize: true,
-        entities: ["src/entities/*.ts"],
+        entities: ["src/entities/**/*.ts"],
       }
     : {
         type: "postgres",
