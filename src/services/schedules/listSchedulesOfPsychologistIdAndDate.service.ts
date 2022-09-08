2f17320f6c5a024480c4afa40f153320e829bcf9
AlexFlavio <alexflavio.dev@gmail.com>
2022-09-08 14:45:48 -0300

    fix: listSchedulesOfPsychologitstIdAndDate , foram feitas alterações para o funcionamento do mesmo, foi alterado o querybuild por query normal.

--- src/services/schedules/listSchedulesOfPsychologistIdAndDate.service.ts
+++ src/services/schedules/listSchedulesOfPsychologistIdAndDate.service.ts
@@ -1,5 +1,4 @@
 import AppDataSource from "../../data-source";
-import { Psychologist } from "../../entities/psychologist.entity";
 import { AppError } from "../../errors/appError";
 
 const listSchedulesOfPsychologistIdAndDateService = async (
@@ -9,16 +8,14 @@ const listSchedulesOfPsychologistIdAndDateService = async (
   if (!date || !psychologistId) {
     throw new AppError(400, "Invalid date or psychologist_id");
   }
-  const schedules = await AppDataSource.getRepository(Psychologist)
-    .createQueryBuilder("psychologist")
-    .innerJoinAndSelect("psychologist.schedules", "schedules")
-    .select(["schedules.*"])
-    .where("psychologist.id = :id , schedules.date = :date", {
-      id: psychologistId,
-      date: date,
-    })
-    .getMany();
-  if (!schedules) {
+  const schedules = await AppDataSource.query(
+    `
+  SELECT schedules.* FROM schedules
+  WHERE schedules."psychologistId" = $1 AND schedules.date = $2 AND schedules.available = true
+  `,
+    [psychologistId, date]
+  );
+  if (schedules.length === 0) {
     throw new AppError(404, "Don't have available schedules in this day");
   }
   return schedules;
