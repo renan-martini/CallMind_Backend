126c86db394a8c88bdf514ef033e5fb8b40def15
SusimaraRoberti <susimararoberti@outlook.com>
2022-09-12 16:41:30 -0300

    fix: inseridos os AppErrors da rota

--- src/services/users/listUser.service.ts
+++ src/services/users/listUser.service.ts
@@ -2,28 +2,44 @@ import AppDataSource from "../../data-source";
 import { Patient } from "../../entities/patient.entity";
 import { Psychologist } from "../../entities/psychologist.entity";
 import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/appError";
 import { patient, psychologist } from "../../utils/utils";
 
 const listUserService = async (userId: string) => {
-  const userRepository = AppDataSource.getRepository(User);
-  const user = await userRepository.findOneBy({ id: userId });
+  try {
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneBy({ id: userId });
 
-  if (user?.type === patient) {
-    const patientsRepository = AppDataSource.getRepository(Patient);
-    const patient = await patientsRepository.findOne({
-      relations: { user: true },
-      where: { user: { id: userId } },
-    });
+    if (!user) {
+      throw new AppError(404, "User not found!");
+    }
 
-    return patient;
-  }
-  if (user?.type === psychologist) {
-    const psychologistsRepository = AppDataSource.getRepository(Psychologist);
-    const psychologist = await psychologistsRepository.findOne({
-      relations: { user: true },
-      where: { user: { id: userId } },
-    });
-    return psychologist;
+    if (user?.type === patient) {
+      const patientsRepository = AppDataSource.getRepository(Patient);
+      const patient = await patientsRepository.findOne({
+        relations: { user: true },
+        where: { user: { id: userId } },
+      });
+
+      return patient;
+    }
+    if (user?.type === psychologist) {
+      const psychologistsRepository = AppDataSource.getRepository(Psychologist);
+      const psychologist = await psychologistsRepository.findOne({
+        relations: { user: true },
+        where: { user: { id: userId } },
+      });
+      return psychologist;
+    }
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw new AppError(error.statusCode, error.message);
+    } else {
+      if (error instanceof Error) {
+        throw new AppError(400, error.message);
+      }
+      throw new AppError(400, "Error");
+    }
   }
 };
 
