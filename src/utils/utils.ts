e726fd6e94f6f2b0089a6e3338f360cb819d3487
AlexFlavio <alexflavio.dev@gmail.com>
2022-09-12 10:07:41 -0300

    feat: nodemailerBase, feita base para utilização do nodemailer, no .env.example estão 3 itens novos que tem de ser adicionados ao .env

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,14 @@
+import "dotenv/config";
+
 export const patient = "Paciente";
 export const psychologist = "Psicólogo";
+
+const nodemailer = require("nodemailer");
+
+export const transporter = nodemailer.createTransport({
+  service: process.env.CM_EMAIL_SERVICE,
+  auth: {
+    user: process.env.CM_EMAIL,
+    pass: process.env.CM_EMAIL_PASS,
+  },
+});
