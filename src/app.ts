fa63c5bfbd8f8fe4bbf0c878a5578087fd68409b
SusimaraRoberti <susimararoberti@outlook.com>
2022-09-12 12:29:49 -0300

    feat: instalando cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,10 @@ import express from "express";
 import { appRoutes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 appRoutes(app);
 app.use(errorMiddleware);
